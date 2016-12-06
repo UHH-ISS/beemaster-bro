9f23b69688f540582fcdfc5b9416cf136dd1aa20
Florian Wilkens <gh@1wilkens.org>
2016-12-06 17:44:32 +0100

    Removed obsolete -a arguments in start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker build . -t bro-image
-docker run --name bro-container --rm -a stderr -a stdout -v /var/log/bro-master/:/usr/local/bro/logs/ -p 9999:9999 bro-image
+docker run --name bro-container --rm -v /var/log/bro-master/:/usr/local/bro/logs/ -p 9999:9999 bro-image
