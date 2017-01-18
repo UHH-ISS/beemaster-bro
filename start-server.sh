7890ba125b80633a777994db130042c1ea04317b
Hauke <4preuss@informatik.uni-hamburg.de>
2017-01-18 23:53:09 +0100

    Update Readme and shell script
    Add run command

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker build . -t bro-image --build-arg PURPOSE=master
-docker run --name bro-master --rm -v /var/beemaster/log/bro-master/:/usr/local/bro/logs/ bro-image
+docker run --name bro-master --rm -v /var/beemaster/log/bro-master/:/usr/local/bro/logs/ bro-image "/bro/scripts_master/"
