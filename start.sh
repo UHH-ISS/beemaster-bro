9548e54f480155591dcd3467cab42775b513b273
Hauke <4preuss@informatik.uni-hamburg.de>
2017-01-19 09:43:20 +0100

    Rename docker master image

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build . -t bro-image --build-arg PURPOSE=master
-docker run --name bro-master --rm -v /var/beemaster/log/bro-master/:/usr/local/bro/logs/ -p 9999:9999 bro-image "/bro/scripts_master/"
+docker build . -t bro-master --build-arg PURPOSE=master
+docker run --name bro-master --rm -v /var/beemaster/log/bro-master/:/usr/local/bro/logs/ -p 9999:9999 bro-master "/bro/scripts_master/"
