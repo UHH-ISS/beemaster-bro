4438bbe2ad64cf75f2d6646edca52cff0e52cdc3
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2017-02-06 19:22:33 +0100

    start scripts for master(update) + slave(new)

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build . -t bro-master --build-arg PURPOSE=master
-docker run --name bro-master --rm -v /var/beemaster/log/bro-master/:/usr/local/bro/logs/ -p 9999:9999 bro-master "/bro/scripts/"
+docker build . -t master --build-arg PURPOSE=master
+docker run -p 9999:9999 --name ma -e MASTER_PUBLIC_PORT=9999 -e MASTER_PUBLIC_IP=172.17.0.1 --rm master
